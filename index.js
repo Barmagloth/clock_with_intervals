3500480226ea5f2e479b3c211ef287af03566686
Barmagloth <5877459+Barmagloth@users.noreply.github.com>
2025-04-13 01:11:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import PerfectClockApp from './App';
+import PerfectClockApp from './clock_eng';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
